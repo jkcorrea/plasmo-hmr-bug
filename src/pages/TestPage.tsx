4d42bd014fe06226fc85ef50f99f49766e668706
Jake Correa <jcorrea257@gmail.com>
2023-02-28 11:41:04 -0800

    update test

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,18 +1,13 @@
 const TestPage = () => {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center">
-      <h1>Change me</h1>
-      <p>{'data'}</p>
+      <h1 className="text-xl">Change me</h1>
     </div>
   )
 }
 
 export const blah = () => {
-  return delay(1000, 'this breaks hmr')
+  console.log('exporting this breaks HMR')
 }
 
 export default TestPage
-
-function delay(ms: number, ...args: any[]) {
-  return new Promise((resolve) => setTimeout(resolve, ms, args))
-}
